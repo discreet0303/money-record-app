bedb32a7c19e04bb6c862bca98e0c52348bd095b
Hsuan <qhespqhesp@gmail.com>
2020-09-09 17:59:52 +0800

    fix rename app error

--- android/app/src/main/java/com/mocord/MainActivity.java
+++ android/app/src/main/java/com/mocord/MainActivity.java
@@ -1,4 +1,4 @@
-package com.moneyrecordproject;
+package com.mocord;
 
 import com.facebook.react.ReactActivity;
 
@@ -10,6 +10,6 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "MoneyRecordProject";
+    return "MoCord";
   }
 }
--- ios/MoCordTests/MoCordTests.m
+++ ios/MoCordTests/MoCordTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface MoneyRecordProjectTests : XCTestCase
+@interface MoCordTests : XCTestCase
 
 @end
 
-@implementation MoneyRecordProjectTests
+@implementation MoCordTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
